ef35997b2782246a4bb312cb7da5dfaf535f72c1
Grigoriy <ligreen@yandex.ru>
2020-04-14 11:44:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Moe Flask приложение в контейнере Docker.'
+    return 'Moe Flask в контейнере Docker.'
  
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
